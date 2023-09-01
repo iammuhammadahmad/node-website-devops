6c8bd57a9ff4d7a73a1018adf16ae17a372bcd79
iammuhammadahmad <m.ahmadsakhawat@gmail.com>
2023-09-01 19:44:08 +0500

    Update deploy.yaml and start_Server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-# Login as root
-sudo -i
+
+echo "Start Application"
 
 APPLICATION_NAME="node-website"
-cd  /var/www/node-website
 
-pm2 describe "$APPLICATION_NAME"
+sudo pm2 describe "$APPLICATION_NAME"
 if [ $? -eq 0 ]
 then
-    pm2 restart "$APPLICATION_NAME"
+    sudo pm2 restart "$APPLICATION_NAME"
 else
-    pm2 start npm --name "$APPLICATION_NAME" -- run start
+    sudo pm2 start npm --name "$APPLICATION_NAME" -- run start
 fi
-pm2 startup systemd
-pm2 save
-systemctl restart apache2
+sudo pm2 startup systemd
+sudo pm2 save
